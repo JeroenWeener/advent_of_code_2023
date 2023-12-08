0fc422629902ffc5045e7ee78e10018a647083aa
Jeroen Weener <jweener1996@gmail.com>
2023-12-08 09:08:51 +0100

    Add solutions for day 8

---
+++ lib/src/solutions/day8_part1.dart
@@ -0,0 +1,81 @@
+import 'package:advent_of_code_2023/advent_of_code_2023.dart';
+
+/// --- Day 8: Haunted Wasteland ---
+///
+/// You're still riding a camel across Desert Island when you spot a sandstorm
+/// quickly approaching. When you turn to warn the Elf, she disappears before
+/// your eyes! To be fair, she had just finished warning you about ghosts a few
+/// minutes ago.
+///
+/// One of the camel's pouches is labeled "maps" - sure enough, it's full of
+/// documents (your puzzle input) about how to navigate the desert. At least,
+/// you're pretty sure that's what they are; one of the documents contains a
+/// list of left/right instructions, and the rest of the documents seem to
+/// describe some kind of network of labeled nodes.
+///
+/// It seems like you're meant to use the left/right instructions to navigate
+/// the network. Perhaps if you have the camel follow the same instructions, you
+/// can escape the haunted wasteland!
+///
+/// After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You
+/// feel like AAA is where you are now, and you have to follow the left/right
+/// instructions until you reach ZZZ.
+///
+/// This format defines each node of the network individually. For example:
+///
+///   RL
+///
+///   AAA = (BBB, CCC)
+///   BBB = (DDD, EEE)
+///   CCC = (ZZZ, GGG)
+///   DDD = (DDD, DDD)
+///   EEE = (EEE, EEE)
+///   GGG = (GGG, GGG)
+///   ZZZ = (ZZZ, ZZZ)
+///
+/// Starting with AAA, you need to look up the next element based on the next
+/// left/right instruction in your input. In this example, start with AAA and go
+/// right (R) by choosing the right element of AAA, CCC. Then, L means to choose
+/// the left element of CCC, ZZZ. By following the left/right instructions, you
+/// reach ZZZ in 2 steps.
+///
+/// Of course, you might not find ZZZ right away. If you run out of left/right
+/// instructions, repeat the whole sequence of instructions as necessary: RL
+/// really means RLRLRLRLRLRLRLRL... and so on. For example, here is a situation
+/// that takes 6 steps to reach ZZZ:
+///
+///   LLR
+///
+///   AAA = (BBB, BBB)
+///   BBB = (AAA, ZZZ)
+///   ZZZ = (ZZZ, ZZZ)
+///
+/// Starting at AAA, follow the left/right instructions. How many steps are
+/// required to reach ZZZ?
+void main() async {
+  List<String> puzzleInput = await Aoc.puzzleInputStrings;
+  List<String> testInput = await Aoc.testInputStrings;
+
+  assert(solve(testInput) == 2);
+
+  int answer = solve(puzzleInput);
+  print(answer);
+}
+
+int solve(List<String> input) {
+  String directions = input.first;
+  String location = 'AAA';
+  int step = 0;
+  while (location != 'ZZZ') {
+    int direction = directions[step % directions.length] == 'L' ? 0 : 1;
+    location = input
+        .skip(2)
+        .firstWhere((e) => e.startsWith(location))
+        .regexAllMatches(r'\w{3}')
+        .skip(1)
+        .elementAt(direction);
+    step++;
+  }
+
+  return step;
+}
